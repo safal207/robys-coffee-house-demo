24c7ac219447a5fcf31ee6b5802cf019cea8fee3
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-07-21 14:51:01 +0300

    chore: bump photo logo stylesheet revision

--- bootstrap.js
+++ bootstrap.js
@@ -32,7 +32,7 @@ installAppleTouchIcon();
 
 const photoLogo = document.createElement("link");
 photoLogo.rel = "stylesheet";
-photoLogo.href = "brand-photo-logo.css?v=20260720-1";
+photoLogo.href = "brand-photo-logo.css?v=20260721-type-1";
 document.head.append(photoLogo);
 
 if (!installAndroidButtonLogo()) {
