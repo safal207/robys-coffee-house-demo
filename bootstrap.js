f4bfc36d687155ca505846a91869d290575f5c7a
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-07-21 12:51:01 +0300

    feat: load photo-referenced Roby's logo

--- bootstrap.js
+++ bootstrap.js
@@ -30,6 +30,11 @@ function installAndroidButtonLogo() {
 
 installAppleTouchIcon();
 
+const photoLogo = document.createElement("link");
+photoLogo.rel = "stylesheet";
+photoLogo.href = "brand-photo-logo.css?v=20260720-1";
+document.head.append(photoLogo);
+
 if (!installAndroidButtonLogo()) {
   let attempts = 0;
   let timeoutId;
