5b30b201ec0938a2a7fe81a7b212678c07615e8c
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-07-22 22:34:45 +0300

    fix: revision shared UX stylesheet URL

--- bootstrap.js
+++ bootstrap.js
@@ -32,7 +32,7 @@ installAppleTouchIcon();
 
 const photoLogo = document.createElement("link");
 photoLogo.rel = "stylesheet";
-photoLogo.href = "brand-photo-logo.css?v=20260721-svg-master-1";
+photoLogo.href = "brand-photo-logo.css?v=20260722-ux238-master-1";
 document.head.append(photoLogo);
 
 if (!installAndroidButtonLogo()) {
