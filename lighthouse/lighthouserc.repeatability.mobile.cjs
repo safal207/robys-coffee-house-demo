ccf096c1281fded3035c5dec33c5151aea2c3329
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-07-30 07:01:52 +0300

    test: add explicit mobile warm-up run

--- lighthouse/lighthouserc.repeatability.mobile.cjs
+++ lighthouse/lighthouserc.repeatability.mobile.cjs
@@ -2,7 +2,7 @@ const base = require('./lighthouserc.mobile.cjs');
 const config = JSON.parse(JSON.stringify(base));
 
 config.ci.collect.url = ['http://localhost/index.html'];
-config.ci.collect.numberOfRuns = 6;
+config.ci.collect.numberOfRuns = 7;
 config.ci.upload = { target: 'filesystem', outputDir: '.artifacts/lighthouse-repeatability/mobile/upload' };
 
 module.exports = config;
