52a9b2837c6696e4035fa9bd85c7f6e82e30d712
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-07-30 07:02:06 +0300

    test: add explicit desktop warm-up run

--- lighthouse/lighthouserc.repeatability.desktop.cjs
+++ lighthouse/lighthouserc.repeatability.desktop.cjs
@@ -2,7 +2,7 @@ const base = require('./lighthouserc.desktop.cjs');
 const config = JSON.parse(JSON.stringify(base));
 
 config.ci.collect.url = ['http://localhost/index.html'];
-config.ci.collect.numberOfRuns = 6;
+config.ci.collect.numberOfRuns = 7;
 config.ci.upload = { target: 'filesystem', outputDir: '.artifacts/lighthouse-repeatability/desktop/upload' };
 
 module.exports = config;
